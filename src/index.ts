fc2b1439891890d880c7e316ab2b4b63d763f358
Urip Subekti <urip.subekti13@gmail.com>
2021-04-01 16:12:01 +0700

    add lodash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Surah } from "./intefaces";
 import quranJSON from "./quran.json";
+import _ from "lodash";
 
 class QuranKemenag {
   constructor() {}
@@ -30,26 +31,35 @@ class QuranKemenag {
       verses_offset: null,
     }
   ): Promise<Surah> {
-    const surah = (quranJSON as any).find(
-      (surah) => surah.surah_id === surah_id
-    );
+    const surah = (quranJSON as any)[surah_id - 1];
+    const verses: any[] = _.cloneDeep(surah.verses) || [];
+    if (!surah) {
+      throw new Error("invalid surah_id, must be between 1-114");
+    }
     if (options.include_verse === false) {
       delete surah.verses;
+      return surah;
     } else {
       let pagination: any = {};
-      if (options.verses_limit) pagination.limit = options.verses_limit;
-      if (options.verses_offset) pagination.offset = options.verses_offset;
-      const verses = (surah.verses || []).slice(
+      if (options.verses_limit !== null)
+        pagination.limit = options.verses_limit;
+      if (options.verses_offset !== null)
+        pagination.offset = options.verses_offset;
+
+      surah.verses = _.slice(
+        verses,
         pagination.offset || 0,
         (pagination.offset || 0) + (pagination.limit || 300)
       );
-      surah.verses = verses;
       if (pagination.limit || pagination.offset) {
         surah.pagination = pagination;
       }
+      return surah;
     }
-    return surah;
   }
 }
 
 export = QuranKemenag;
+
+// const quran = new QuranKemenag();
+// quran.getSurah(1).then(console.log);
